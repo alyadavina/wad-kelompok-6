4579ea759dae213982ece308f7e30d094a40f3bf
Davyhilbram <davyhilbram57@gmail.com>
2025-05-30 15:56:12 +0700

    Davy Comit 2 -Friday 15.56

---
+++ database/migrations/2025_05_30_051902_create_notifikasi_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifikasi', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('mahasiswa_id');
+            $table->string('judul');
+            $table->text('isi');
+            $table->boolean('is_read')->default(false);
+            $table->timestamps();
+
+            $table->foreignId('mahasiswa_id')->references('id')->on('mahasiswa')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifikasi');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware('auth:mahasiswa')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');})->name('dashboard'); 
+    });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('beasiswa', BeasiswaController::class)->middleware('auth');
+require __DIR__.'/auth.php';
+
+Route::resource('beasiswa', BeasiswaController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/favorite', [FavoriteController::class, 'store'])->name('favorits.store');
+Route::get('/favorite', [FavoriteController::class, 'index'])->name('favorits.index');
+
+Route::middleware('auth:mahasiswa')->group(function () {
+    Route::get('/notifikasi', [NotifikasiController::class, 'index'])->name('notifikasi.index');
+    Route::post('/notifikasi/{id}/read', [NotifikasiController::class, 'markAsRead'])->name('notifikasi.markAsRead');
+});
+
+
+
